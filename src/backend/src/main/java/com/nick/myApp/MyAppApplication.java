dc7455aacee913bd1156677ca7c207f3f6486f2f
NickWWK <wingkin0713@gmail.com>
2026-06-13 22:12:42 +0800

    Update files

--- src/backend/src/main/java/com/nick/myApp/MyAppApplication.java
+++ src/backend/src/main/java/com/nick/myApp/MyAppApplication.java
@@ -2,8 +2,10 @@ package com.nick.myApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.nick.myApp.models")
 public class MyAppApplication {
 
 	public static void main(String[] args) {
